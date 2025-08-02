dfdc8895cfbd61d194c4f7b77a160ba3797234ee
IamAdinath <adinathgore.17@gmail.com>
2025-08-02 16:48:52 +0530

    update app and home with footer component

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Preloader from "../components/Preloader";
 import Banner from "../components/Banner";
 import Features from "../components/Features";
-
+import Footer from "../components/Footer";
 const Home: React.FC = () => {
   return (
     <>
       <Preloader />
       <Banner />
       <Features />
+      <Footer />
 
     </>
   );
