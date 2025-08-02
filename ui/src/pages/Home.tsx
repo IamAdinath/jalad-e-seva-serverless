dc2da187f4d34ad59153f09eeb87a0e8f015b1de
IamAdinath <adinathgore.17@gmail.com>
2025-08-02 16:04:21 +0530

    remove unused codes

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Preloader from "../components/Preloader";
-import Header from "../components/Header";
 import Banner from "../components/Banner";
 import Features from "../components/Features";
-// Import About, Services, ContactForm, Footer after creation
 
 const Home: React.FC = () => {
   return (
