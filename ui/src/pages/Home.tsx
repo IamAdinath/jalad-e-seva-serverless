7ccd4423d65218185821bf9c8e1cada66169a157
IamAdinath <adinathgore.17@gmail.com>
2025-08-02 17:26:35 +0530

    include marqee in pages

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -3,11 +3,21 @@ import Preloader from "../components/Preloader";
 import Banner from "../components/Banner";
 import Features from "../components/Features";
 import Footer from "../components/Footer";
+import UpdatesMarquee from "../components/UpdatesMarquee";
+
 const Home: React.FC = () => {
+  const latestUpdates = [
+  "New scholarship programs for 2025 have been announced.",
+  "The deadline for agricultural loan applications is approaching.",
+  "Public transport services will be updated starting next month.",
+  "Check your eligibility for the new housing scheme now."
+];
+
   return (
     <>
       <Preloader />
       <Banner />
+      <UpdatesMarquee updates={latestUpdates} />
       <Features />
       <Footer />
     </>
