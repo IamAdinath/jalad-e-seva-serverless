7226a01c7bea950c8f877628c7fc9a3fe24be2d4
IamAdinath <adinathgore.17@gmail.com>
2025-08-02 13:01:13 +0530

    Lambda update the references

--- api/lambda/users/confirm.py
+++ api/lambda/users/confirm.py
@@ -18,17 +18,17 @@ def lambda_handler(event, context):
     try:
         if not os.environ.get('USER_POOL_ID'):
             logger.error('USER_POOL_ID not set')
-            return build_response(StatusCodes.INTERNAL_SERVER_ERROR, Headers.CORS, {'message': 'USER_POOL_ID not set'})
+            return build_response(StatusCodes.INTERNAL_SERVER_ERROR, Headers.INTERNAL_SERVER_ERROR, {'message': 'USER_POOL_ID not set'})
         payload = json.loads(event.get('body'))
         if not payload.get('username'):
             logger.error('username not set')
-            return build_response(StatusCodes.BAD_REQUEST, Headers.CORS, {'message': 'username not set'})
+            return build_response(StatusCodes.BAD_REQUEST, Headers.BAD_REQUEST, {'message': 'username not set'})
         client = boto3.client('cognito-idp')
         client.admin_confirm_sign_up(
             UserPoolId=os.environ['USER_POOL_ID'],
             Username=payload['username']
         )
-        return build_response(StatusCodes.OK, Headers.CORS, {'message': 'User confirmed'})
+        return build_response(StatusCodes.OK, Headers.Default, {'message': 'User confirmed'})
     except Exception as e:
         logger.error(e)
-        return build_response(StatusCodes.INTERNAL_SERVER_ERROR, Headers.CORS, {'message': str(e)})
+        return build_response(StatusCodes.INTERNAL_SERVER_ERROR, Headers.INTERNAL_SERVER_ERROR, {'message': str(e)})
