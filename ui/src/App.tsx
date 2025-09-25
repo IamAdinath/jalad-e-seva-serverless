c4705693be4549603dbb393f722763cc7c6d2d7a
IamAdinath <adinathgore.17@gmail.com>
2025-09-25 23:57:04 +0530

    replace alerts by toasters

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -5,22 +5,26 @@ import CategoryBlogs from './pages/CategoryBlogs';
 import AllBlogs from './pages/AllBlogs';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Blog from './pages/Blog';
+import { ToastProvider, ToastContainer } from './components/Toast';
 
 
 function App() {
   return (
-    <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<AllBlogs />} />
-        <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/create-post" element={<CreatePost />} />
-        {/* All other paths are treated as categories */}
-        <Route path="/:category" element={<CategoryBlogs />} />
-      </Routes>
-    </Router>
-    </div>
+    <ToastProvider>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blogs" element={<AllBlogs />} />
+            <Route path="/blog/:id" element={<Blog />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            {/* All other paths are treated as categories */}
+            <Route path="/:category" element={<CategoryBlogs />} />
+          </Routes>
+        </Router>
+        <ToastContainer />
+      </div>
+    </ToastProvider>
   )
 }
 
---
+++ ui/src/components/Toast/ToastContext.tsx
@@ -0,0 +1,82 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { ToastType } from './Toast';
+
+export interface ToastData {
+  id: string;
+  message: string;
+  type: ToastType;
+  duration?: number;
+}
+
+interface ToastContextType {
+  toasts: ToastData[];
+  addToast: (message: string, type: ToastType, duration?: number) => void;
+  removeToast: (id: string) => void;
+  showSuccess: (message: string, duration?: number) => void;
+  showError: (message: string, duration?: number) => void;
+  showWarning: (message: string, duration?: number) => void;
+  showInfo: (message: string, duration?: number) => void;
+}
+
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+export const useToast = () => {
+  const context = useContext(ToastContext);
+  if (!context) {
+    throw new Error('useToast must be used within a ToastProvider');
+  }
+  return context;
+};
+
+interface ToastProviderProps {
+  children: ReactNode;
+}
+
+export const ToastProvider: React.FC<ToastProviderProps> = ({ children }) => {
+  const [toasts, setToasts] = useState<ToastData[]>([]);
+
+  const addToast = (message: string, type: ToastType, duration = 5000) => {
+    const id = Math.random().toString(36).substr(2, 9);
+    const newToast: ToastData = { id, message, type, duration };
+    
+    setToasts((prevToasts) => [...prevToasts, newToast]);
+  };
+
+  const removeToast = (id: string) => {
+    setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+  };
+
+  const showSuccess = (message: string, duration?: number) => {
+    addToast(message, 'success', duration);
+  };
+
+  const showError = (message: string, duration?: number) => {
+    addToast(message, 'error', duration);
+  };
+
+  const showWarning = (message: string, duration?: number) => {
+    addToast(message, 'warning', duration);
+  };
+
+  const showInfo = (message: string, duration?: number) => {
+    addToast(message, 'info', duration);
+  };
+
+  const value: ToastContextType = {
+    toasts,
+    addToast,
+    removeToast,
+    showSuccess,
+    showError,
+    showWarning,
+    showInfo,
+  };
+
+  return (
+    <ToastContext.Provider value={value}>
+      {children}
+    </ToastContext.Provider>
+  );
+};
+
+export default ToastProvider;
